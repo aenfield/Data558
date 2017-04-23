ffd75e09c07e50c6193da7aed14c7fa7ff6aaad5
Andrew Enfield <aenfield@gmail.com>
2017-04-23 13:25:01 -0700

    Finished first tests and impl of coordinate descent.

--- Week04/coorddescent-test.py
+++ Week04/coorddescent-test.py
@@ -3,7 +3,7 @@ from coorddescent import *
 
 import numpy as np
 
-# simple data, with which to test
+# simple data, for testing
 y = np.array([2, 3, 0, - 1])
 x = np.array([4, -2, 1,
               5, -3, 2,
@@ -11,7 +11,7 @@ x = np.array([4, -2, 1,
               0, 1, 4]).reshape(4, 3)
 lam = 1
 beta = np.zeros(3)
-beta_iter_2 = np.array([0.536, 0, 0])
+beta_iter_2 = np.array([0.5357142857142857, 0, 0])
 
 
 class CyclicCoordDescentTest(unittest.TestCase):
@@ -20,11 +20,32 @@ class CyclicCoordDescentTest(unittest.TestCase):
 
     def test_one_iteration_with_one_feature(self):
         beta_updated = cycliccoorddescent(x, y, lam, max_iter=1)
-        np.testing.assert_allclose(beta_updated, np.array([0.536, 0, 0], rtol=1e-3))
+        np.testing.assert_allclose(beta_updated, np.array([0.536, 0, 0]), rtol=1e-3)
+
+    def test_one_iteration_with_three_features(self):
+        beta_updated = cycliccoorddescent(x, y, lam, max_iter=3)
+        np.testing.assert_allclose(beta_updated, np.array([0.536, -0.1143, -0.1574]), rtol=1e-3)
+
+    def test_one_iteration_with_five_iterations(self):
+        beta_updated = cycliccoorddescent(x, y, lam, max_iter=5)
+        np.testing.assert_allclose(beta_updated, np.array([0.540, -0.1191, -0.1574]), rtol=1e-3)
+
+    def test_one_iteration_with_six_iterations(self):
+        beta_updated = cycliccoorddescent(x, y, lam, max_iter=6)
+        np.testing.assert_allclose(beta_updated, np.array([0.540, -0.1191, -0.1597]), rtol=1e-3)
 
 
+    def test_sequence_of_js_just_one(self):
+        seq = get_sequence_of_js(3, 1)
+        np.testing.assert_allclose(seq, np.array([1]))
 
-#        np.testing.assert_allclose(beta_updated, np.array([0.536, -0.1143, -0.1573], rtol=1e-3))
+    def test_sequence_of_js_with_three(self):
+        seq = get_sequence_of_js(3, 3)
+        np.testing.assert_allclose(seq, np.arange(1,4))
+
+    def test_sequence_of_js_with_two_sets(self):
+        seq = get_sequence_of_js(3, 6)
+        np.testing.assert_allclose(seq, np.append(np.arange(1,4), np.arange(1,4)))
 
 
 
@@ -36,7 +57,7 @@ class MinimizationTest(unittest.TestCase):
 
     def test_c_term_second_index(self):
         c = c_term(beta_iter_2, x, y, 2, lam)
-        np.testing.assert_allclose(c, -4.416)
+        np.testing.assert_allclose(c, -4.429, rtol=1e-3)
 
     def test_a_term_with_simple_data(self):
         a = a_term(x, 1)
@@ -48,7 +69,7 @@ class MinimizationTest(unittest.TestCase):
 
     def test_minimize_beta_term_negative_c(self):
         b_hat = minimize_beta_term(beta_iter_2, x, y, 2, lam)
-        np.testing.assert_allclose(b_hat, -0.1139, rtol=1e-03)
+        np.testing.assert_allclose(b_hat, -0.1143, rtol=1e-03)
 
     def test_minimize_beta_term_midrange_c(self):
         # big lambda so the c term is inside it
--- Week04/coorddescent.py
+++ Week04/coorddescent.py
@@ -27,13 +27,24 @@ def minimize_beta_term(beta, x, y, j, lam):
         return 0
 
 
+def get_sequence_of_js(feature_count, iterations):
+    # get a sequence of feature indices/j values of size max_iter
+    # we repeat enough times to have at least the right size or more,
+    # then filter to just the right amount
+    repeat_features_count = (iterations // feature_count) + 1
+    sequence_of_js = np.repeat(np.arange(1, feature_count+1)[np.newaxis], repeat_features_count, 0).flatten()[:iterations]
+
+    return sequence_of_js
+
+
 def cycliccoorddescent(x, y, lam, max_iter=500):
     feature_count = x.shape[1]
 
-    # get a sequence of feature indices/j values
-    # we repeat enough times to have at least or more numbers,
-    # then filter to just the right amount
-    repeat_features_count = (max_iter / feature_count) + 1
-    sequence_of_js = np.repeat(y[np.newaxis], 2, 0).flatten()[:max_iter]
+    beta = np.zeros(feature_count)
+    sequence_of_js = get_sequence_of_js(feature_count, max_iter)
 
     for j in sequence_of_js:
+        b_hat_for_j = minimize_beta_term(beta, x, y, j, lam)
+        beta[j-1] = b_hat_for_j # j indices are 1-based, so subtract one to set correct coef
+
+    return beta
