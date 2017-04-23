c13478930b357b5046e417771a9035bf28fac99d
Andrew Enfield <aenfield@gmail.com>
2017-04-23 08:49:49 -0700

    Finished first test, passing, for c term.

--- Week04/coorddescent.py
+++ Week04/coorddescent.py
@@ -1,18 +1,11 @@
 import numpy as np
 
 def c_term(beta, x, y, j, lam):
-    
-    
-    grad_beta = (-2/len(x) * (x.T).dot((y - x.dot(beta))))
-    grad_beta = grad_beta + 2*lam*beta
-    return grad_beta
-        
-def computeobject():
-    pass
-    
-def graddescent():
-    pass
-    
-def fastgradalgo():
-    pass
-    
+    # list of items to keep, with item j removed
+    idxs_to_keep = list(range(x.shape[1]))
+    idxs_to_keep.remove(j-1)
+
+    beta_minus_j = beta[idxs_to_keep]
+    x_minus_j = x[:, idxs_to_keep]
+
+    return 2 * sum(x[:, 0] * y - x_minus_j.dot(beta_minus_j))
