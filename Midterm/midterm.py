c4fa4bad472419b04659a1940d3ccf0e38be756d
Andrew Enfield <aenfield@gmail.com>
2017-05-16 21:59:47 -0700

    Finished first cut at all but the optimization for elastic net, including text. Added text and initial stuff for PCA.

--- Midterm/midterm.py
+++ Midterm/midterm.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
+import copy
 
+# ----
+# Elastic net
+# ----
 
 # this is the equivalent of my computeobj function
 def elasticnet_objective(beta, x, y, lam, alpha):
@@ -77,3 +81,62 @@ def randcoorddescent(x, y, lam, alpha, max_iter=500):
 def get_final_coefs(vals_dataframe):
     """Return the last row of a set of coefficients (like those returned by graddescent)."""
     return vals_dataframe[-1:].values[0,:]  # the [0,:] turns a (1,2) into a (2,)
+
+
+# ----
+# PCA via Oja
+# ----
+
+
+def oja_fit(Z, component_count, eta_0, t_0, num_epochs):
+    """
+    Return an array of arrays where each array is a principal component vector. Uses the Oja algorithm. I
+    put this convenience routine together based on Corinne's lab code.
+    """
+    a_0 = np.random.randn(np.size(Z, 1))  # starting point
+    a_0 /= np.linalg.norm(a_0, axis=0)
+    v1, _ = oja(copy.deepcopy(Z), a_0, eta_0, t_0, num_epochs)  # Run the algorithm for first component vector
+
+    princ_comps = v1[np.newaxis]
+    # newaxis because right now we're only returning one and we want it to be element zero in an outer array
+
+    Z_new = copy.deepcopy(Z)
+    v_curr = v1
+    for i in range(1, component_count):
+        Z_new = deflate(Z_new, v_curr)
+        v_curr, _ = oja(copy.deepcopy(Z_new), a_0, eta_0, t_0, num_epochs)
+
+        princ_comps = np.concatenate( [princ_comps, v_curr[np.newaxis]] )
+
+    return(princ_comps)
+
+
+def oja(Z, a_0, eta_0, t_0, num_epochs):
+    """
+    Implements the normalized Oja algorithm to produce the first PCA eigenvector (and then we use deflate to 
+    get the second and later eigenvectors). I worked through the code from Corinne below, based on the lab,
+    and understand how it operates.
+    """
+    t = 0
+    a = a_0
+    n = np.size(Z, 0)
+    lambdas = np.zeros(num_epochs)
+    for epoch in range(0, num_epochs):
+        # Shuffle the rows of the data after each epoch
+        np.random.shuffle(Z)
+        for i in range(0, n):
+            # Note it's faster not to compute the matrix ZZ^T
+            last_a = a
+            a = a + eta_0/(t+t_0)*np.dot(Z[i,], np.dot(Z[i,].T, a))
+            a = a/np.linalg.norm(a)
+
+            a_delta = last_a - a
+            mean_a_delta = np.mean(a_delta)
+
+            t += 1
+        lambdas[epoch] = a.dot(Z.T).dot(Z).dot(a)/n
+
+    return a, lambdas
+
+def deflate(Z, a):
+    return Z - Z.dot(np.outer(a, a))
