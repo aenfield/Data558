b01edd9e9b3110bfa0905ae9f8684e87a71ee525
Andrew Enfield <aenfield@gmail.com>
2017-05-24 18:36:54 -0700

    Finished impl of gradient and objective funcs for linear SVM.

--- Week08/linearsvm-test.py
+++ Week08/linearsvm-test.py
@@ -3,15 +3,36 @@ from linearsvm import *
 
 import numpy as np
 
-# simple data for tests
+# simple data for some of the tests
 x_simple = np.array([3,2,0,1,-1,-2]).reshape(3,2)
 y_simple = np.array([1,0,0])
-beta_init_simple = np.zeros(2)
 
 class LinearSvmFunctionsTest(unittest.TestCase):
-    def test_computelinearsvmgrad_with_simpledata(self):
-        betas = computelinearsvmgrad(beta_init_simple, x_simple, y_simple, 1)
-        np.testing.assert_approx_equal(betas, np.array([-2, -4/3]))
+    def test_gradient_with_simpledata(self):
+        betas = compute_linearsvm_gradient(np.zeros(2), x_simple, y_simple, 1)
+        np.testing.assert_array_almost_equal(betas, np.array([-2, -4/3]))
+
+    def test_gradient_with_nonzero_simpledata(self):
+        betas = compute_linearsvm_gradient(np.array([1,1]), x_simple, y_simple, 1)
+        np.testing.assert_array_almost_equal(betas, np.array([2, 2]))
+
+    def test_gradient_with_nonzero_simpledata_with_fewer_zeros(self):
+        betas = compute_linearsvm_gradient(np.array([1,-1]), x_simple, np.array([3,2,1]), 1)
+        np.testing.assert_array_almost_equal(betas, np.array([2, -6]))
+
+    def test_gradient_with_nonzero_simpledata_and_diff_lambda(self):
+        betas = compute_linearsvm_gradient(np.array([1,-1]), x_simple, np.array([3,2,1]), 3)
+        np.testing.assert_array_almost_equal(betas, np.array([6, -10]))
+
+
+    def test_objective_with_simpledata(self):
+        np.testing.assert_array_almost_equal(compute_linearsvm_objective(np.zeros(2), x_simple, y_simple, 1), 1)
+
+    def test_objective_with_nonzero_simpledata(self):
+        np.testing.assert_array_almost_equal(compute_linearsvm_objective(np.array([1,1]), x_simple, y_simple, 1), 2.667, decimal=3)
+
+    def test_objective_with_nonzero_simpledata_and_diff_lambda(self):
+        np.testing.assert_array_almost_equal(compute_linearsvm_objective(np.array([1,1]), x_simple, y_simple, 3), 6.667, decimal=3)
 
 
 class GradientImplementationTest(unittest.TestCase):
--- Week08/linearsvm.py
+++ Week08/linearsvm.py
@@ -1,20 +1,19 @@
 import numpy as np
 
 
-def computelinearsvmgrad(beta, x, y, lam):
-    return np.zeros(2)
-    # grad_beta = (-2 / len(x) * (x.T).dot((y - x.dot(beta))))
-    # grad_beta = grad_beta + 2 * lam * beta
-    # return grad_beta
+def compute_linearsvm_gradient(beta, x, y, lam):
+    grad_beta_max_term = np.maximum(0, 1 - (y * (x.dot(np.array(beta)))))[np.newaxis].T
+    grad_beta_sum_term = y[np.newaxis].T * x * grad_beta_max_term
+    grad_beta_without_penalty = (-2 / len(x)) * np.sum(grad_beta_sum_term, 0)
+    grad_beta_penalty = 2 * lam * beta
+    return grad_beta_without_penalty + grad_beta_penalty
 
+def compute_linearsvm_objective(beta, x, y, lam):
+    objective_max_term = np.maximum(0, 1 - (y * (x.dot(np.array(beta)))))[np.newaxis].T
+    obj_without_penalty = (1 / len(x)) * np.sum(objective_max_term**2)
+    obj_penalty = lam * np.sum(beta**2)
 
-def computeobject():
-    pass
-
-
-def graddescent():
-    pass
-
+    return obj_without_penalty + obj_penalty
 
 def fastgradalgo():
     pass
