3e3bf98a84e1c8311a95d2cc46a6e83c9df36a5f
Andrew Enfield <aenfield@gmail.com>
2017-05-30 17:43:39 -0700

    Finished first cut at kerneleval_linear, and MNIST data load impl.

--- Week09/kernelsvm-test.py
+++ Week09/kernelsvm-test.py
@@ -6,7 +6,7 @@ from sklearn.metrics.pairwise import linear_kernel, polynomial_kernel
 
 # simple data for some of the tests
 x_simple = np.array([3,2,0,1,-1,-2]).reshape(3,2)
-y_simple = np.array([1,0,0])
+y_simple = np.array([1,1,-1])
 
 class KernelSvmFunctionsTest(unittest.TestCase):
     def test_computegram_linear_with_simpledata(self):
@@ -18,7 +18,38 @@ class KernelSvmFunctionsTest(unittest.TestCase):
         np.testing.assert_array_almost_equal(gram, polynomial_kernel(x_simple, degree=3, coef0=1, gamma=1))
         # we use gamma = 1 to match what we want, overriding the function's default that normalizes based on n
 
-    def
+    def test_kerneleval_linear_with_simpledata(self):
+        vec = kerneleval_linear(x_simple, np.array([2,2]))
+        np.testing.assert_array_almost_equal(vec, np.array([10,2,-6]))
+
+    # TODO need to write/test kerneleval_polynomial?
+
+    def test_gradient_with_simpledata(self):
+        betas = compute_kernelsvm_gradient(np.zeros(2), x_simple, y_simple, 1)
+        np.testing.assert_array_almost_equal(betas, np.array([-2, -4/3]))
+
+    def test_gradient_with_nonzero_simpledata(self):
+        betas = compute_kernelsvm_gradient(np.array([1,1]), x_simple, y_simple, 1)
+        np.testing.assert_array_almost_equal(betas, np.array([2, 2]))
+
+    def test_gradient_with_nonzero_simpledata_with_fewer_zeros(self):
+        betas = compute_kernelsvm_gradient(np.array([1,-1]), x_simple, np.array([3,2,1]), 1)
+        np.testing.assert_array_almost_equal(betas, np.array([2, -6]))
+
+    def test_gradient_with_nonzero_simpledata_and_diff_lambda(self):
+        betas = compute_kernelsvm_gradient(np.array([1,-1]), x_simple, np.array([3,2,1]), 3)
+        np.testing.assert_array_almost_equal(betas, np.array([6, -10]))
+
+
+    def test_objective_with_simpledata(self):
+        np.testing.assert_array_almost_equal(compute_kernelsvm_objective(np.zeros(2), x_simple, y_simple, 1), 1)
+
+    def test_objective_with_nonzero_simpledata(self):
+        np.testing.assert_array_almost_equal(compute_kernelsvm_objective(np.array([1,1]), x_simple, y_simple, 1), 2.667, decimal=3)
+
+    def test_objective_with_nonzero_simpledata_and_diff_lambda(self):
+        np.testing.assert_array_almost_equal(compute_kernelsvm_objective(np.array([1,1]), x_simple, y_simple, 3), 6.667, decimal=3)
+
 
 if __name__ == '__main__':
     unittest.main()
--- Week09/kernelsvm.py
+++ Week09/kernelsvm.py
@@ -16,3 +16,15 @@ def computegram_polynomial(X, degree, coef0):
     return (X.dot(X.T) + coef0)**degree
     # we should/could use the canned implementation, but we'll do this manually to show we can
     #return linear_kernel(X)
+
+def kerneleval_linear(X, new_observation):
+    """
+    Evaluate new_observation compared to the existing observations, using a linear kernel
+    """
+    return new_observation.dot(X.T)
+
+def compute_kernelsvm_gradient():
+    return 0
+
+def compute_kernelsvm_objective():
+    return 0
