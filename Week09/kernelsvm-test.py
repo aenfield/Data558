e557f6d579ed9d9a64bf827feed3fedcff4ad41f
Andrew Enfield <aenfield@gmail.com>
2017-05-31 09:24:04 -0700

    Check in to move on to project - i'll come back to this; see TODOs in the test file.

--- Week09/kernelsvm-test.py
+++ Week09/kernelsvm-test.py
@@ -4,9 +4,34 @@ from kernelsvm import *
 import numpy as np
 from sklearn.metrics.pairwise import linear_kernel, polynomial_kernel
 
+from sklearn.datasets import fetch_mldata
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
 # simple data for some of the tests
 x_simple = np.array([3,2,0,1,-1,-2]).reshape(3,2)
 y_simple = np.array([1,1,-1])
+
+# TODOs
+# 1a. Figure out what's going on when I call fastgradalgo with my two functions (w/ and w/o backtracking?). Based on
+# one quick check it seems like it runs - very slowly, takes noticeable time even with just 10 iterations and no
+# backtracking - but doesn't converge. I can try running it in PyCharm w/ the MNIST data below, so I can step through
+# and see what's happening and what's taking time.
+# 1b. (if it seems like 1a is working then perhaps that shows that this is ok and doesn't need more work) After
+# confirming with Corinne or others that my by-hand understanding of the gradient implementation is accurate, make
+# sure that the tests I have a) have the correct values by hand and then b) that the
+# 2. Need an implementation of kerneleval, in some fashion, for the polynomial kernel - however, I don't need to
+# do it with kerneleval - I really just need a predict function that does the kerneleval step and the alpha weighting
+# and summing.
+
+
+# MNIST data to check something
+# mnist = fetch_mldata('MNIST original')
+# ones_and_eights = (mnist.target == 1) | (mnist.target == 8)
+# X = mnist.data[ones_and_eights]
+# y = mnist.target[ones_and_eights]
+# X_scaled = preprocessing.scale(X)
+# X_scaled_train, X_scaled_test, y_train, y_test = train_test_split(X_scaled, y)
 
 class KernelSvmFunctionsTest(unittest.TestCase):
     def test_computegram_linear_with_simpledata(self):
@@ -22,34 +47,37 @@ class KernelSvmFunctionsTest(unittest.TestCase):
         vec = kerneleval_linear(x_simple, np.array([2,2]))
         np.testing.assert_array_almost_equal(vec, np.array([10,2,-6]))
 
-    # TODO need to write/test kerneleval_polynomial?
+    # TODO need to write/test kerneleval_polynomial? Yep - We'll need this for accuracy w/ the polynomial kernel,
+    # however it could be in a separate function that just does predict and also does the weighting via alpha
 
     def test_gradient_with_simpledata(self):
-        betas = compute_kernelsvm_gradient(np.zeros(3), computegram_linear(x_simple), y_simple, 1)
-        np.testing.assert_array_almost_equal(betas, np.array([-2, -4/3]))
+        alphas = compute_kernelsvm_gradient(np.zeros(3), computegram_linear(x_simple), y_simple, 1)
+        np.testing.assert_array_almost_equal(alphas, np.array([-14.67, -3.33, 9.33]), 2)
 
-    # def test_gradient_with_nonzero_simpledata(self):
-    #     betas = compute_kernelsvm_gradient(np.array([1,1,1]), x_simple, y_simple, 1)
-    #     np.testing.assert_array_almost_equal(betas, np.array([2, 2]))
-    #
-    # def test_gradient_with_nonzero_simpledata_with_fewer_zeros(self):
-    #     betas = compute_kernelsvm_gradient(np.array([1,-1,1]), x_simple, np.array([3,2,1]), 1)
-    #     np.testing.assert_array_almost_equal(betas, np.array([2, -6]))
-    #
-    # def test_gradient_with_nonzero_simpledata_and_diff_lambda(self):
-    #     betas = compute_kernelsvm_gradient(np.array([1,-1,1]), x_simple, np.array([3,2,1]), 3)
-    #     np.testing.assert_array_almost_equal(betas, np.array([6, -10]))
-    #
-    #
+    def test_gradient_with_nonzero_simpledata(self):
+        alphas = compute_kernelsvm_gradient(np.array([1,1,1]), computegram_linear(x_simple), y_simple, 1)
+        np.testing.assert_array_almost_equal(alphas, np.array([16, 2, -8]))
+
+    def test_gradient_with_nonzero_simpledata_with_fewer_zeros(self):
+        alphas = compute_kernelsvm_gradient(np.array([1,-1,1]), computegram_linear(x_simple), np.array([3,2,1]), 1)
+        np.testing.assert_array_almost_equal(alphas, np.array([4.67, -4.67, 4.67]), 2)
+
+    def test_gradient_with_nonzero_simpledata_and_diff_lambda(self):
+        alphas = compute_kernelsvm_gradient(np.array([1,-1,1]), computegram_linear(x_simple), np.array([3,2,1]), 3)
+        np.testing.assert_array_almost_equal(alphas, np.array([20.67, -8.67, 4.67]), 2)
+
+
     def test_objective_with_simpledata(self):
-        np.testing.assert_array_almost_equal(compute_kernelsvm_objective(np.zeros(3), computegram_linear(x_simple), y_simple, 1), 1)
+        self.assertEqual(compute_kernelsvm_objective(np.zeros(3), computegram_linear(x_simple), y_simple, 1), 1)
 
-    # def test_objective_with_nonzero_simpledata(self):
-    #     np.testing.assert_array_almost_equal(compute_kernelsvm_objective(np.array([1,1,1]), x_simple, y_simple, 1), 2.667, decimal=3)
-    #
-    # def test_objective_with_nonzero_simpledata_and_diff_lambda(self):
-    #     np.testing.assert_array_almost_equal(compute_kernelsvm_objective(np.array([1,1,1]), x_simple, y_simple, 3), 6.667, decimal=3)
+    def test_objective_with_nonzero_simpledata(self):
+        self.assertEqual(compute_kernelsvm_objective(np.array([1,1,1]), computegram_linear(x_simple), y_simple, 1), 5)
 
+    def test_objective_with_nonzero_simpledata_and_diff_lambda(self):
+        self.assertEqual(compute_kernelsvm_objective(np.array([1,1,1]), computegram_linear(x_simple), y_simple, 3), 15)
+
+
+    # def test
 
 if __name__ == '__main__':
     unittest.main()
--- Week09/kernelsvm.py
+++ Week09/kernelsvm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel, polynomial_kernel
 
 def computegram_linear(X):
@@ -24,14 +25,72 @@ def kerneleval_linear(X, new_observation):
     return new_observation.dot(X.T)
 
 def compute_kernelsvm_gradient(alphas, K, y, lam):
-    grad_beta_max_term = np.maximum(0, 1 - (y * (K.dot(np.array(alphas)))))[np.newaxis].T
+    grad_beta_max_term = np.maximum(0, 1 - (y * (K.dot(alphas))))[np.newaxis].T
     grad_beta_sum_term = y[np.newaxis].T * K * grad_beta_max_term
     grad_beta_without_penalty = (-2 / len(y)) * np.sum(grad_beta_sum_term, 0)
     grad_beta_penalty = 2 * lam * (K.dot(alphas))
     return grad_beta_without_penalty + grad_beta_penalty
 
 def compute_kernelsvm_objective(alphas, K, y, lam):
-    objective_max_term = np.maximum(0, 1 - (y * (K.dot(np.array(alphas)))))[np.newaxis].T
+    objective_max_term = np.maximum(0, 1 - (y * (K.dot(alphas))))[np.newaxis].T
     obj_without_penalty = (1 / len(y)) * np.sum(objective_max_term**2)
-    obj_penalty = lam * alphas * (K.dot(alphas))
+    obj_penalty = lam * (alphas.T.dot(K.dot(alphas)))
     return obj_without_penalty + obj_penalty
+
+# this is my implementation from homework three
+def backtracking(coefs, x, y, grad_func, obj_func, t=1,
+                 alpha=0.5, beta=0.5, max_iter=100, lam=5):
+    """
+    Returns a value of t, for use w/ gradient descent, obtained via
+    backtracking."""
+    grad_coefs = grad_func(coefs, x, y, lam)
+    norm_grad_coefs = np.linalg.norm(grad_coefs) # norm of gradient
+    found_t = False
+    iter = 0
+    while (found_t == False and iter < max_iter):
+        attempted_value = obj_func(coefs - (t * grad_coefs),
+                                   x, y, lam)
+        compare_value = obj_func(coefs, x, y,
+                                 lam) - (alpha * t * norm_grad_coefs**2)
+        if attempted_value < compare_value:
+          found_t = True
+        elif iter == max_iter:
+            # should ideally check whether 'exit' is valid
+            exit("Backtracking: max iterations reached")
+        else:
+            t = t * beta
+            iter = iter + 1
+
+    return t
+    #return(t, iter)
+
+# my implementation from homework three
+def fastgradalgo(x, y, t_init, grad_func, obj_func,
+                 max_iter=100, lam=5, t_func=None):
+    """
+    Implement the fast gradient descent algorithm. Uses t_func 
+    to determine t, or uses a fixed size of t_init if
+    t_func is None. That is, pass in a function like 
+    'backtracking' to do a line search.
+    """
+    beta = np.zeros(x.shape[1])  # shape[1] is the number of columns
+    theta = beta.copy()  # we init theta to zeros too
+    t = t_init
+    beta_vals = pd.DataFrame(beta[np.newaxis, :])
+
+    for i in range(0, max_iter):
+        if t_func:
+            t = t_func(beta, x, y, grad_func, obj_func, t_init)
+
+        beta_prev = beta.copy()
+        beta = theta - (t * grad_func(theta, x, y, lam))
+        theta = beta + (i / (i + 3)) * (beta - beta_prev)
+
+        beta_vals = beta_vals.append(pd.DataFrame(beta[np.newaxis]),
+                                     ignore_index=True)
+
+    return beta_vals
+
+# my implementation from homework three
+def get_final_coefs(beta_vals):
+    return beta_vals[-1:].values
