e2f2facf92f734d430adf518770204325f1aab17
Andrew Enfield <aenfield@gmail.com>
2017-06-01 09:33:48 -0700

    Added short module to convert pickled files to csv.

---
+++ Kaggle/pickled_files_to_csv.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+# Loads the pickled files created by images_to_features.py and saves the contents as csv files, to avoid
+# issues with different versions of pickle, different processor architectures, etc.
+
+def main():
+    files = ['features_train', 'labels_train', 'features_test', 'labels_test']
+
+    for file in files:
+        print("Processing {}.".format(file))
+        data_from_pickle = np.array(pickle.load(open(file, 'rb')))
+        output_file_path = '{}.csv'.format(file)
+        pd.DataFrame(data_from_pickle).to_csv(output_file_path, header=False, index=False)
+        print("Saved {}.".format(output_file_path))
+
+if __name__ == "__main__":
+   main()
